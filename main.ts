d1ee16c0b6702d3d0f2c0b4edcd91f9d6832b831
RoboGort <87195667+RoboGort@users.noreply.github.com>
2021-07-19 17:40:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 basic.showString("Hola a tots i totes")
 basic.showString("Aquests és un fitxer per saber si es pot compartir online un fitxer fet amb micro:bit")
+basic.showString("3a linia gusrfst a github")
 basic.forever(function () {
     basic.showIcon(IconNames.Yes)
 })
