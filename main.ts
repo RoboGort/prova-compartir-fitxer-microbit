6fe4f77936c25be8454b5e70c700e51e2ae3ba4a
RoboGort <87195667+RoboGort@users.noreply.github.com>
2021-07-19 17:19:09 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showString("Hola a tots i totes")
+basic.showString("Aquests és un fitxer per saber si es pot compartir online un fitxer fet amb micro:bit")
+basic.forever(function () {
+    basic.showIcon(IconNames.Yes)
+})
